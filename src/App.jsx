417bf6cb7ad079824456cb386676f7b9686afd40
zulukho0 <zulukho0@gmail.com>
2025-10-02 21:47:32 +0200

    Update routing on Appp.jsc touse Protected Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import DashboardPage from "./pages/DashboardPage";
-import StudentsPage from "./pages/StudentsPage";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage.jsx';
+import DashboardPage from './pages/DashboardPage.jsx';
+import StudentsPage from './pages/StudentsPage.jsx';
+import ProtectedRoute from './components/ProtectedRoute.jsx';
+import { AuthProvider } from './context/AuthContext.jsx';
 
-function App() {
+export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/students" element={<StudentsPage />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* Public */}
+          <Route path="/" element={<LoginPage />} />
+
+          {/* Protected */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/students" element={<StudentsPage />} />
+            {/* Future:
+            <Route path="/courses" element={<CoursesPage />} />
+            <Route path="/classes" element={<ClassesPage />} />
+            <Route path="/results" element={<ResultsPage />} />
+            <Route path="/instructors" element={<InstructorsPage />} />
+            */}
+          </Route>
+
+          {/* Fallback */}
+          <Route path="*" element={<div className="p-6">Not found</div>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
-
-export default App;
-
