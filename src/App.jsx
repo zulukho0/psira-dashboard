65286b1c429283cbd6303af452125c400c36a342
zulukho0 <zulukho0@gmail.com>
2025-10-13 18:27:20 +0200

    feat: add classes page route and import

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import DashboardPage from './pages/DashboardPage.jsx';
 import StudentsPage from './features/students/pages/StudentsPage.jsx';
 import CoursesPage from './features/courses/pages/CoursesPage.jsx';
 import InstructorsPage from './features/instructors/pages/InstructorsPage.jsx';
+import ClassesPage from './features/classes/pages/ClassesPage.jsx';
 import ProtectedRoute from './components/ProtectedRoute.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
 
@@ -21,6 +22,7 @@ export default function App() {
             <Route path="/students" element={<StudentsPage />} />
             <Route path="/courses" element={<CoursesPage />} />
             <Route path="/instructors" element={<InstructorsPage />} />
+            <Route path="/classes" element={<ClassesPage />} />
           </Route>
 
 
