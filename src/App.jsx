b819b7fc464ae9804d73735c8fca5ec7b7112413
zulukho0 <zulukho0@gmail.com>
2025-10-15 13:33:24 +0200

    feat(courses): add manage subjects page route

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import CoursesPage from './features/courses/pages/CoursesPage.jsx';
 import InstructorsPage from './features/instructors/pages/InstructorsPage.jsx';
 import ClassesPage from './features/classes/pages/ClassesPage.jsx';
 import ManageStudentsPage from './features/classes/pages/ManageStudentsPage.jsx';
+import ManageSubjectsPage from './features/courses/pages/ManageSubjectsPage.jsx';
 import ProtectedRoute from './components/ProtectedRoute.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
 
@@ -25,6 +26,7 @@ export default function App() {
             <Route path="/instructors" element={<InstructorsPage />} />
             <Route path="/classes" element={<ClassesPage />} />
             <Route path="/classes/:id/students" element={<ManageStudentsPage />} />
+            <Route path="/courses/:courseId/subjects" element={<ManageSubjectsPage />} />
           </Route>
 
 
